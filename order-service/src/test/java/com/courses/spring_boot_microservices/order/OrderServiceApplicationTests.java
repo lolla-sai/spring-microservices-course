6abeb15b749ad8602ba8a9cfb44c547f6acfc496
lolla-sai <saisameer.lolla@gmail.com>
2025-11-13 23:05:09 +0530

    Complete Order Service

--- order-service/src/test/java/com/courses/spring_boot_microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/courses/spring_boot_microservices/order/OrderServiceApplicationTests.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 class OrderServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
